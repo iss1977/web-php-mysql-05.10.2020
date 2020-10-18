85572f47b919d8e784f7628c6cfb6fd0b4077f42
iss1977 <ioan.szucs@gmail.com>
2020-10-18 20:40:25 +0200

    aws mysql connection implemented

--- inc/login.inc.php
+++ inc/login.inc.php
@@ -1,8 +1,8 @@
 <?php
-$host ="localhost";
-$pw = "123456";
-$user ="sebastian";
-$db = "php_beispiel_05102020";
+$host ="18.159.190.97";
+$pw = "myuserpass";
+$user ="myuser";
+$db = "mydb";
 
 $con = new mysqli($host, $user, $pw, $db);
 if($con->connect_errno) {
